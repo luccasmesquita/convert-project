ba8e0d099f4b7f5917748413ab99f263b4485af3
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2024-12-05 11:42:16 +0000

    Obtendo a moeda selecionada

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
+//Obtendo os elementos dos formulario 
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
 
 // Manipulando o input amount para receber somente numeros 
 amount.addEventListener("input", () => {
     const hasCharactersRegex = /\D+/g;
     amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+// Captando o evento de submit (enviar) no formulario 
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    console.log(currency.value)
+}
