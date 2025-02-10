e7b6693b513bce2e9f77ba0cac7a020c8e77c27c
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2025-02-10 18:31:17 +0000

    exibindo footer

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ const BTC = 619.602;
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
 
 // Manipulando o input amount para receber somente numeros 
 amount.addEventListener("input", () => {
@@ -38,5 +39,15 @@ form.onsubmit = (event) => {
 
 // Função para converter a moeda 
 function convertCurrency(amount, price, symbol) {
-    console.log(amount, price, symbol)
+    try {       
+        // aplica a classe que exibe o footer para mostrar o resultado 
+        footer.classList.add("show-result")
+    } catch (error) {
+        // remove a classe do footer removendo ele da tela
+        footer.classList.remove("show-result")
+        
+        console.log(error)
+        alert("Não foi possível converter. Tente novamente mais tarde.")
+
+    }
 }
